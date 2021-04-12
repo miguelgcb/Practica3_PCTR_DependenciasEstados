d3c564e7176d7b47a635d54c86354f7c047f37ec
Miguel Garcia <mgb1002@alu.ubu.es>
2021-04-12 09:59:30 +0200

    Actividad Salida Puerta Changes

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,6 +1,22 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+public class ActividadSalidaPuerta implements Runnable{
+	
+	
+	private static final int NUMENTRADAS = 20;
+	private String puerta;
+	private IParque parque;
+
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	//
 	// TODO
