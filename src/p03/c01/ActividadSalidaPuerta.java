e5a8ec0b9d16ca3060a0473a401976edab8d4d69
afp1002 <afp1002@alu.ubu.es>
2021-04-13 11:17:31 +0200

    modificacion clase actividadsalidapuerta

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -7,30 +7,29 @@ import java.util.logging.Logger;
 
 public class ActividadSalidaPuerta implements Runnable {
 
-	private static final int NUMENTRADAS = 20;
-	private String puerta;
 	private IParque parque;
+    private String puerta;
+    private static final int NUMSALIDAS = 20;
 
-	public ActividadSalidaPuerta(String puerta, IParque parque) {
-		this.puerta = puerta;
-		this.parque = parque;
-	}
+    public ActividadSalidaPuerta(IParque parque,String puerta) {
+        this.parque=parque;
+        this.puerta=puerta;
+    }
 
-	@Override
-	public void run() {
-		for (int i = 0; i < NUMENTRADAS; i++) {
-			try {
-				parque.salirDelParque(puerta);
-				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 1000);
-			} catch (InterruptedException e) {
-				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
-				Logger.getGlobal().log(Level.INFO, e.toString());
-				return;
-			}
-		}
-	}
+    @Override
+    public void run() {
+        for(int i=0;i<NUMSALIDAS;i++) {
 
-	//
-	// TODO
-	//
+            try {
+            	parque.salirDelParque(puerta);
+                TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 1000);
+            } catch (InterruptedException e) {
+            	Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+                Logger.getGlobal().log(Level.INFO, e.toString());
+                return;
+            }
+
+        }
+
+    }
 }
