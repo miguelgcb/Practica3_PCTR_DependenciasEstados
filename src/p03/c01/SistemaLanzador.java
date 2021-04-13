5b66d2f842493057548746fcaf653e22970b053b
Miguel Garcia <mgb1002@alu.ubu.es>
2021-04-13 11:24:32 +0200

    Cambios en el Main Arreglado instanciacion de Parque

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -3,7 +3,7 @@ package src.p03.c01;
 public class SistemaLanzador {
 	public static void main(String[] args) {
 
-		IParque parque = new Parque(); // TODO
+		Parque parque = new Parque(); 
 		char letra_puerta = 'A';
 
 		System.out.println("¡Parque abierto!");
